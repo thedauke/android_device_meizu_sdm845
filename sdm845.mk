a789459901fb4000ee7870824deab86a63e89a6c
Roman Kiryanov <rkir@google.com>
2020-04-08 14:23:47 -0700

    sdm845: Replace audio@2.0-service with audio.service
    Legacy service name, use android.hardware.audio.service instead
    
    Bug: 153579444
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Merged-In: I4c05accb86dd0c9cd1b3fef3d84e0d54b2afc6a1
    Change-Id: I6b55b4b1db2577f4176c99dca7021cfe78e13307

--- sdm845.mk
+++ sdm845.mk
@@ -71,7 +71,7 @@ PRODUCT_CHARACTERISTICS := nosdcard
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio.effect@5.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.audio@4.0-impl.meizu_sdm845 \
     android.hardware.soundtrigger@2.2-impl \
     audio.a2dp.default \
