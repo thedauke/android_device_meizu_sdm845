4d5eb63d2e3af096ab553af1d6363822e622f96f
TRANSAERO21 <martynenkodima21@gmail.com>
2021-04-11 17:23:41 +0600

    sdm845: sepolicy: Give all needs permissions for LOC fastcharge HAL :white_check_mark:

--- m1882/BoardConfig.mk
+++ m1882/BoardConfig.mk
@@ -163,6 +163,8 @@ VENDOR_SECURITY_PATCH := 2020-10-01
 # SELinux
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/public
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(DEVICE_PATH)
