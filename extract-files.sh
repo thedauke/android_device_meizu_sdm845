d8759e57469af5cb119c0074f96495ca2cc16698
TRANSAERO21 <martynenkodima21@gmail.com>
2021-04-11 17:34:17 +0600

    sdm845: proprietary: avoid using libshims if possible :link::arrow_right:

--- extract-files.sh
+++ extract-files.sh
@@ -27,7 +27,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 DEV_ROOT="${MY_DIR}/../../.."
 
-HELPER="${DEV_ROOT}/vendor/aosp/build/tools/extract_utils.sh"
+HELPER="${DEV_ROOT}/tools/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -75,24 +75,23 @@ function blob_fixup() {
     vendor/bin/hw/vendor.display.color@1.0-service | vendor/bin/hw/vendor.qti.hardware.qteeconnector@1.0-service | vendor/lib/vendor.display.postproc@1.0_vendor.so)
         patchelf --remove-needed "android.hidl.base@1.0.so" "${2}"
         ;;
-
     vendor/lib64/hw/vendor.qti.hardware.sensorscalibrate@1.0-impl.so)
         sed -i "s|libbase.so|libbv28.so|g" "${2}"
         ;;
-
     vendor/lib/hw/camera.qcom.so)
         sed -i "s|libssc.so|libSSc.so|g" "${2}"
         ;;
     vendor/lib/libmms_hal_vstab.so | vendor/lib/libmms_warper_vstab.so)
-
         patchelf --add-needed "libshim_camera.so" "${2}"
-
         ;;
-
     vendor/lib/hw/camera.qcom.so)
-
         patchelf --add-needed "libprotobuf-cpp-full-vendor-3.9.1.so" "${2}"
-
+        ;;
+    etc/vstab_db_0_720p_video_30fps.config | etc/vstab_db_0_4k_video_30fps.config | etc/vstab_db_0_1080p_video_30fps.config | vendor/lib/camera/components/com.arcsoft.node.realtimebokeh.so | vendor/lib/camera/components/com.qti.stats.pdlib.so | vendor/lib/camera/components/com.arcsoft.node.capturebokeh.so | vendor/lib/camera/components/com.arcsoft.node.picauto.so | vendor/lib/camera/components/com.inv.node.eis.so | vendor/lib/camera/components/com.arcsoft.node.hdr.so | vendor/lib/camera/components/com.qti.stats.aec.so | vendor/lib/camera/components/com.arcsoft.node.beauty.so | vendor/lib/camera/components/com.arcsoft.node.smoothtransition.so | vendor/lib/camera/components/com.arcsoft.node.lowlighthdr.so | vendor/lib/libmms_hal_vstab.so | vendor/lib/libarcsoft_dualcam_refocus.so | vendor/lib/hw/com.qti.chi.override.so | vendor/lib/hw/camera.qcom.so)
+        sed -i "s|/data/misc/camera|/data/vendor/cumX|g" "${2}"
+        ;;
+    vendor/etc/perf/commonresourceconfigs.xml | vendor/etc/perf/perfboostsconfig.xml | vendor/etc/perf/perfconfigstore.xml | vendor/etc/perf/targetconfig.xml | vendor/etc/perf/targetresourceconfigs.xml)
+        sed -i "s|Target=\"sdm710\"|Target=\"sdm845\"|g" "${2}"
         ;;
     esac
 }
