c20546d1e3a206f78174244fde48f5dd54b7ac46
TRANSAERO21 <martynenkodima21@gmail.com>
2021-05-26 09:15:04 +0000

    sdm845: fixup some packages name

--- sdm845.mk
+++ sdm845.mk
@@ -221,7 +221,7 @@ PRODUCT_PACKAGES += \
 
 # Media
 PRODUCT_PACKAGES += \
-    libavservices_minijail.vendor \
+    libavservices_minijail_vendor \
     libmediaplayerservice
 
 PRODUCT_COPY_FILES += \
@@ -242,7 +242,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-		android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
