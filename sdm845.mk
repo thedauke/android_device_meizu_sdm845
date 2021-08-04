f079aab1b13bd9c97c0d70ce6d4d753135321faa
TRANSAERO21 <martynenkodima21@gmail.com>
2021-08-05 02:43:22 +0700

    sdm845: Remove libhidl makefiles from device tree
    it's present in ./system/libhidl/...

--- sdm845.mk
+++ sdm845.mk
@@ -161,7 +161,6 @@ PRODUCT_COPY_FILES += \
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
-    android.hidl.base@1.0_system \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
