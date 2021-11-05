05d9fa9b39c95a1db920ced1c3181e0647d054d1
TRANSAERO21 <martynenkodima21@gmail.com>
2021-11-05 22:33:13 +0300

    sdm845: display: Move DisplayConfig.h to dedicated module same way as Tonemapper.h

--- display/libdisplayconfig/Android.mk
+++ display/libdisplayconfig/Android.mk
@@ -1,10 +1,15 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_COPY_HEADERS_TO     := $(common_header_export_path)
+LOCAL_COPY_HEADERS        := DisplayConfig.h
+LOCAL_VENDOR_MODULE       := true
+include $(BUILD_COPY_HEADERS)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE                  := libdisplayconfig
 LOCAL_MODULE_TAGS             := optional
 LOCAL_HEADER_LIBRARIES        := display_headers
-LOCAL_COPY_HEADERS            := DisplayConfig.h
 LOCAL_SRC_FILES               := DisplayConfig.cpp
 LOCAL_SHARED_LIBRARIES        := libhidlbase libhidltransport libutils \
                                  vendor.display.config@1.0 android.hidl.base@1.0
